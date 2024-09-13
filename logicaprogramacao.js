232b70e664c16f1a241c288777fd0fdce982ac87
Évelim Dornelles <dorrrnelles@gmail.com>
2024-09-13 12:18:23 -0300

    classificador

---
+++ logicaprogramacao.js
@@ -0,0 +1,19 @@
+let experienciaHeroi = 100001;
+
+if (experienciaHeroi < 1000) {
+    console.log("Ferro!")
+} else if (experienciaHeroi > 1000 && experienciaHeroi < 2001) {
+    console.log("Bronze")
+} else if (experienciaHeroi > 2001 && experienciaHeroi < 5000) {
+    console.log("Prata Ouro")
+} else if (experienciaHeroi > 5001 && experienciaHeroi < 8000) {
+    console.log("Platina Diamante")
+} else if (experienciaHeroi > 8001 && experienciaHeroi < 9000) {
+    console.log("Ascendente")
+} else if (experienciaHeroi > 9001 && experienciaHeroi < 10000) {
+    console.log("Imortal")
+} else {
+    console.log("Radiante")
+}
+
+console.log(experienciaHeroi)
